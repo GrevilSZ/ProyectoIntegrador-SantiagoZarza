4bf63560e0ed78750f9e00cc366965b1064d00c0
Santiago Zarza <santiagoz@outlook.com>
2022-10-25 15:38:51 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { LogoComponent } from './Components/logo/logo.component';
+import { SocialComponent } from './Components/social/social.component';
+import { BannerComponent } from './Components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
